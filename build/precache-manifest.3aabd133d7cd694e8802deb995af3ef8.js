bc2f51de377e3e17167422721b55377f60b03aef
abdonaber <51922753+abdonaber@users.noreply.github.com>
2020-06-03 18:09:37 +0400

    a

---
+++ build/precache-manifest.3aabd133d7cd694e8802deb995af3ef8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e99eddbae2b746a005d6523ca646354",
+    "url": "/node-react-fullstack/index.html"
+  },
+  {
+    "revision": "613a556aa1af7a9a5f5c",
+    "url": "/node-react-fullstack/static/css/main.4bbbf4ae.chunk.css"
+  },
+  {
+    "revision": "0394d7c995817b252034",
+    "url": "/node-react-fullstack/static/js/2.2b1a9585.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/node-react-fullstack/static/js/2.2b1a9585.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "613a556aa1af7a9a5f5c",
+    "url": "/node-react-fullstack/static/js/main.2e3d828d.chunk.js"
+  },
+  {
+    "revision": "4368fac181b03071b45a",
+    "url": "/node-react-fullstack/static/js/runtime-main.75e7a0dd.js"
+  }
+]);
